31e06e78b18ec7f7330346cdffc781c630c32d6e
caiocsgomes <caiocsgomes@gmail.com>
2022-11-19 18:02:44 -0300

    adding s3 bucket to infra

---
+++ infra/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "caiogomes.me-tf-backend"
+    key    = "state/terraform.tfstate"
+    region = "sa-east-1"
+    dynamodb_table = "caiogomes.me-tf-backend"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "sa-east-1"
+}
---
+++ infra/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "s3_app_bucket" {
+  bucket = "${var.project_name}-app-bucket"
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.s3_app_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.s3_app_bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.s3_app_bucket.arn,
+      "${aws_s3_bucket.s3_app_bucket.arn}/*",
+    ]
+  }
+}
